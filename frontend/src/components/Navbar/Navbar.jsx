0fd9d2c80b08aa705b355d2e13ddb2655f486672
Gautami06 <gautamigupta0620@gmail.com>
2024-08-06 23:54:53 +0530

    Landing page

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+import logo1 from '../../assets/logo1.png'
+
+function Navbar() {
+  return (
+    <nav className='main-nav'>
+        <img
+          className='logo1'
+          src={logo1}
+          alt='logo'
+        />
+        
+        <div className='nav-links'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+
+
+      </nav>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Main.css'
+import CarouselSlide from '../../components/Slider/Slider'
+import HowItWorks from '../../components/Works/Works'
+
+function Main() {
+  return (
+    <>
+     <CarouselSlide/>
+     <HowItWorks/>
+    </>
+  )
+}
+
+export default Main
